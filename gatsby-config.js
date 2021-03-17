f01f028622dbeda9256090b59000bf384eaf82f9
Juliors1 <juliorojassolis@gmail.com>
2021-03-17 12:45:32 -0400

    update information in config

--- gatsby-config.js
+++ gatsby-config.js
@@ -1,8 +1,8 @@
 module.exports = {
   siteMetadata: {
-    title: `Gatsby Default Starter`,
-    description: `Kick off your next, great Gatsby project with this default starter. This barebones starter ships with the main Gatsby configuration files you might need.`,
-    author: `@gatsbyjs`,
+    title: `Julio Rojas | Software Engineer`,
+    description: `Julio's Portfolio built using Gatsby`,
+    author: `Julio Rojas`,
   },
   plugins: [
     `gatsby-plugin-react-helmet`,
@@ -28,7 +28,8 @@ module.exports = {
         icon: `src/images/gatsby-icon.png`, // This path is relative to the root of the site.
       },
     },
-    `gatsby-plugin-gatsby-cloud`,
+    `gatsby-plugin-sass`,
+
     // this (optional) plugin enables Progressive Web App + Offline functionality
     // To learn more, visit: https://gatsby.dev/offline
     // `gatsby-plugin-offline`,
